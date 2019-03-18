40e9f26923e733039c72a6e156194125d65a462b
Constantine Chutis <constantinechutis@gmail.com>
2019-03-18 15:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase() {
+  string === string.toLowerCase()
     return "I can't hear you!";
-  } else if string === string.toUpperCase() {
-    return "YES INDEED!";
-  } else {
-    return "I love you, too";
-  }
 }
