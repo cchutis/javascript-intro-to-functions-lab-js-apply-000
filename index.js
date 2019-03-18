122f6a8c67a9f2649fb4c2f390eb9d8f367534a4
Constantine Chutis <constantinechutis@gmail.com>
2019-03-18 15:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   string === string.toLowerCase()
-    return "I can't hear you!";
+    return "I can't hear you!"
 }
 
 function sayHiToGrandma(string) {
   string === string.toUpperCase()
-    return "YES INDEED!"; 
+    return "YES INDEED!" 
 }
 
 function sayHiToGrandma(string) {
